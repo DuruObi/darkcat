7402cc2a56ed03e6f30c150e8ae3576f8f9db8a8
DuruObi <3878oed@gmail.com>
2026-02-02 16:13:15 +0100

    DarkCat v5.0.0 major developer upgrade

--- MyWebApp/eslint.config.js
+++ MyWebApp/eslint.config.js
@@ -1,31 +1,10 @@
 // DarkCat v5 ESLint config for MyWebApp
 export default [
   {
-    files: ["*.js", "*.mjs"],
-    languageOptions: {
-      ecmaVersion: 2023,
-      sourceType: "module"
-    },
+    files: ["*.js"],
     rules: {
-      "semi": ["error", "always"],
-      "quotes": ["error", "double"],
-      "no-unused-vars": ["warn"],
-      "no-console": ["off"]
-    }
-  },
-  {
-    files: ["*.ts"],
-    languageOptions: {
-      ecmaVersion: 2023,
-      sourceType: "module"
-    },
-    rules: {
-      "semi": ["error", "always"],
-      "quotes": ["error", "double"],
-      "@typescript-eslint/no-unused-vars": ["warn"]
-    },
-    plugins: {
-      "@typescript-eslint": require("@typescript-eslint/eslint-plugin")
+      semi: ["error", "always"],
+      quotes: ["error", "double"]
     }
   }
 ];
